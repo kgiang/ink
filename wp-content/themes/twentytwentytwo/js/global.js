1da5f3cf1742acf9a4d03bbfa400f85d84a830b0
kgiang <giangnk3691@gmail.com>
2022-12-04 16:33:45 +0700

    Revert "Init"
    This reverts commit ab38f0ae1ae2fc0267d86fda07cb18ccdeb5543e.

--- wp-content/themes/twentytwentytwo/js/global.js
+++
@@ -1,8 +0,0 @@
-jQuery(function ($) {
-    $(document).ready(function () {
-        $(document).onClick('#select-price-block', function() {
-            $('.select-block-container').hide();
-            $('.select-price').show();
-        })
-    });
-});
